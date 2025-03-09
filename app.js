a4af1aa4035cc172155fda575353fad8da99b7c8
saraaaksanthya <saraksanthya@gmail.com>
2025-03-09 15:13:51 +0530

    updated

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cors = require('cors'); // Add CORS middleware
+const cors = require('cors');
 const authRoutes = require('./routes/auth');
 const app = express();
 
@@ -12,10 +12,8 @@ mongoose.connect('mongodb://localhost/musicApp', { useNewUrlParser: true, useUni
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors()); // Use CORS middleware
+app.use(cors());
 app.use('/api/auth', authRoutes);
 
-app.use(cors({ origin: 'http://localhost:3001' })); // Allow requests from the frontend URL
-
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server started on port ${PORT}`)); // Listen on all network interfaces
